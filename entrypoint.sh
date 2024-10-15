ce0cf335e5bba0c09fb7c8926c3136476f7e6652
longopy <javier_longobardo@hotmail.com>
2024-10-15 12:07:02 +0200

    Fix last commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ chmod 0644 /etc/cron.d/cron_execution
 cat /etc/cron.d/cron_execution
 
 echo "Running job immediately..."
-python3 /opt/app/main.py >> /var/log/cron.log 2>&1
+cd /opt/app/ && python3 main.py >> /var/log/cron.log 2>&1
 
 echo "Starting cron..."
 crond -l 2 && tail -f /var/log/cron.log
